1e92935a884034d7f8aa92e0d59d9e5ffe4ded9e
Singh <sanskriti.singh@lloydsbanking.dev>
2025-03-19 17:53:29 +0530

    added swagger UI

--- backend/hedera/backend/src/token/token.controller.ts
+++ backend/hedera/backend/src/token/token.controller.ts
@@ -1,78 +1,95 @@
 import { Controller, Post, Body, Get, Query } from '@nestjs/common';
+import { ApiQuery } from '@nestjs/swagger';
 import { TokenService } from './token.service';
+import {
+  CreateTokenDto,
+  MintTokenDto,
+  BurnTokenDto,
+  PurchaseTokenDto,
+  RedeemTokenDto,
+  AssociateTokenDto,
+  DissociateTokenDto,
+  TransferTokenDto,
+  GetAccountAssetDto,
+  GetAssetsDto,
+  GetBalanceDto,
+  GetAccountBalanceDto,
+  GetAccountAssetsDto,
+  GetTransactionHistoryDto
+} from './dto/token.dto'
 
 @Controller('token')
 export class TokenController {
   constructor(private readonly tokenService: TokenService) { }
 
   @Post('create')
-  createToken(@Body() body: { tokenName: string; symbol: string; tokenValue:number}) {
+  createToken(@Body() body: CreateTokenDto) {
     return this.tokenService.createToken(body);
   }
 
   @Post('mint')
-  mintToken(@Body() body: { tokenId: string; amount: number; }) {
+  mintToken(@Body() body: MintTokenDto) {
     return this.tokenService.mintToken(body);
   }
 
   @Post('burn')
-  burnToken(@Body() body: { tokenId:string; amount: number; sender: string }) {
+  burnToken(@Body() body: BurnTokenDto) {
     return this.tokenService.burnToken(body);
   }
 
   @Post('purchase')
-  purchaseToken(@Body() body: { tokenId:string; amount: number; sender: string }) {
+  purchaseToken(@Body() body: PurchaseTokenDto) {
     return this.tokenService.purchaseToken(body);
   }
 
   @Post('redeem')
-  redeemToken(@Body() body: { tokenId:string; amount: number; sender: string }) {
+  redeemToken(@Body() body: RedeemTokenDto) {
     return this.tokenService.redeemToken(body);
   }
 
   @Post('associate')
-  associateToken(@Body() body: { tokenId: string; sender: string; }) {
+  associateToken(@Body() body: AssociateTokenDto) {
     return this.tokenService.associateToken(body);
   }
 
   @Post('dissociate')
-  dissociateToken(@Body() body: { tokenId: string; account: string; sender: string; amount: number }) {
+  dissociateToken(@Body() body: DissociateTokenDto) {
     return this.tokenService.dissociateToken(body);
   }
 
   @Post('transfer')
-  transferToken(@Body() body: { tokenId: string; receiver: string; sender: string, amount: number }) {
+  transferToken(@Body() body: TransferTokenDto) {
     return this.tokenService.transferToken(body);
   }
 
 
   @Get('asset')
-  getAccountAsset(@Query('sender') sender: string, @Query('tokenId') tokenId: string) {
-    return this.tokenService.getAsset(sender, tokenId);
+  getAccountAsset(@Query() query: GetAccountAssetDto) {
+    return this.tokenService.getAsset(query.sender, query.tokenId);
   }
 
   @Get('assets')
-  getAssets(@Query('sender') sender: string) {
-    return this.tokenService.getAssets(sender);
+  getAssets(@Query() query: GetAssetsDto) {
+    return this.tokenService.getAssets(query.sender);
   }
 
   @Get('balance')
-  getBalance(@Query('sender') sender: string) {
-    return this.tokenService.getBalance(sender);
+  getBalance(@Query() query: GetBalanceDto) {
+    return this.tokenService.getBalance(query.sender);
   }
 
   @Get('account-balance')
-  getAccountBalance(@Query('account') account: string) {
-    return this.tokenService.getAccountBalance(account);
+  getAccountBalance(@Query() query: GetAccountBalanceDto) {
+    return this.tokenService.getAccountBalance(query.account);
   }
 
   @Get('account-assets')
-  getAccountAssets(@Query('account') account: string) {
-    return this.tokenService.getAccountAssets(account);
+  getAccountAssets(@Query() query: GetAccountAssetsDto) {
+    return this.tokenService.getAccountAssets(query.account);
   }
 
   @Get('transaction-history')
-  getTransactionHistory(@Query('account') account: string) {
-    return this.tokenService.getTransactionHistory(account);
+  getTransactionHistory(@Query() query: GetTransactionHistoryDto) {
+    return this.tokenService.getTransactionHistory(query.account);
   }
 }
