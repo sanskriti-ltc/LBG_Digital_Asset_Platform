cdcc4209a9d7c674790ec70fa7b5866df7d6fd67
Naveen Neelam <naveen.neelam@lloydsbanking.com>
2025-03-26 15:11:30 +0530

    user details fetching done through ledger

--- backend/canton/backend/src/token/token.controller.ts
+++ backend/canton/backend/src/token/token.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Post, Body, Get, Param } from '@nestjs/common';
 import { TokenService } from './token.service';
 import { MintDto, PurchaseDto, RedeemDto, TransferDto } from './token.dto';
 
@@ -6,23 +6,47 @@ import { MintDto, PurchaseDto, RedeemDto, TransferDto } from './token.dto';
 export class TokenController {
   constructor(private readonly tokenService: TokenService) {}
 
+  @Get()
+  async getParties() {
+    const parties = await this.tokenService.getParties();
+    return parties;
+  }
+
+  @Get("listUsers")
+  async listUsers() {
+    const parties = await this.tokenService.listUsers();
+    return parties;
+  }
+
+  @Get('getUserDetails')
+  async listUserRights() {
+    const parties = await this.tokenService.listUserRights('alice');
+    return parties;
+  }
+
+  @Get(':partyId')
+  async getPartyDetails(@Param('partyId') partyId: string) {
+    const partyDetails = await this.tokenService.getPartyDetails(partyId);
+    return partyDetails;
+  }
+  
   @Post('mint')
   async mintToken(@Body() mintDto: MintDto) {
     return this.tokenService.mintToken(mintDto);
   }
 
-  @Post('transfer')
-  async transferToken(@Body() transferDto: TransferDto) {
-    return this.tokenService.transferToken(transferDto);
-  }
+  // @Post('transfer')
+  // async transferToken(@Body() transferDto: TransferDto) {
+  //   return this.tokenService.transferToken(transferDto);
+  // }
 
-  @Post('redeem')
-  async redeemToken(@Body() redeemDto: RedeemDto) {
-    return this.tokenService.redeemToken(redeemDto);
-  }
+  // @Post('redeem')
+  // async redeemToken(@Body() redeemDto: RedeemDto) {
+  //   return this.tokenService.redeemToken(redeemDto);
+  // }
 
-  @Post('purchase')
-  async purchaseToken(@Body() purchaseDto: PurchaseDto) {
-    return this.tokenService.purchaseToken(purchaseDto);
-  }
+  // @Post('purchase')
+  // async purchaseToken(@Body() purchaseDto: PurchaseDto) {
+  //   return this.tokenService.purchaseToken(purchaseDto);
+  // }
 }
--- backend/canton/backend/src/token/token.service.ts
+++ backend/canton/backend/src/token/token.service.ts
@@ -1,14 +1,39 @@
 import { Injectable } from '@nestjs/common';
 import { Ledger } from '@daml/ledger';
 import { MintDto, TransferDto, RedeemDto, PurchaseDto } from './token.dto';
-import { Token } from '../LbgAssetTokenizationDamlModule/LbgAssetTokenization-0.0.1/lib/Token';
+import { Token } from '@daml.ts/ledger/lib/Token';
+import { escape } from 'querystring';
 
 @Injectable()
 export class TokenService {
   private ledger: Ledger;
+  private token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJodHRwczovL2RhbWwuY29tL2xlZGdlci1hcGkiOnsibGVkZ2VySWQiOiJzYW5kYm94IiwiYXBwbGljYXRpb25JZCI6Ikxsb3lkc0Jhbms6OjEyMjBlZDcxZjIyZDZkNWMyNTM1ODJhYmZkZDRhMzQ1MTk0Mjk0Zjk5YjNhZmJiNWIwZWY4ODYxNzk3MjRiNjMxYzlkIiwiYWN0QXMiOlsibGxveWRzQmFuayJdfX0.ISLaEjS5fqiPLIlJ2BXl--bM0QI7_bmkZZQnSDbJ8QM";
 
   constructor() {
-    this.ledger = new Ledger({ token: 'YOUR_LEDGER_API_TOKEN' });
+    this.ledger = new Ledger({ token: this.token, httpBaseUrl: 'http://localhost:7575/' });
+  }
+
+  async getParties() {
+    const response = await this.ledger.listKnownParties();
+    return response;
+  }
+
+  async listUsers() {
+    const response = await this.ledger.listUsers();
+    return response;
+  }
+
+  async listUserRights(userId: string) {
+    const response = await this.ledger.listUserRights(userId);
+    console.log(response)
+    return response;
+  }
+
+  async getPartyDetails(partyId: string)
+  // : Promise<PartyDetails> 
+  {
+    // const response = await this.ledger.getPartyDetails(partyId);
+    // return response;
   }
 
   async mintToken(mintDto: MintDto) {
@@ -16,18 +41,18 @@ export class TokenService {
     return this.ledger.exercise(Token.Mint, tokenCid, { newAmount });
   }
 
-  async transferToken(transferDto: TransferDto) {
-    const { owner, tokenCid, newOwner, transferAmount } = transferDto;
-    return this.ledger.exercise(Token.Transfer, tokenCid, { newOwner, transferAmount });
-  }
+  // async transferToken(transferDto: TransferDto) {
+  //   const { owner, tokenCid, newOwner, transferAmount } = transferDto;
+  //   return this.ledger.exercise(Token.Transfer, tokenCid, { newOwner, transferAmount });
+  // }
 
-  async redeemToken(redeemDto: RedeemDto) {
-    const { owner, tokenCid, redeemAmount } = redeemDto;
-    return this.ledger.exercise(Token.Redeem, tokenCid, { redeemAmount });
-  }
+  // async redeemToken(redeemDto: RedeemDto) {
+  //   const { owner, tokenCid, redeemAmount } = redeemDto;
+  //   return this.ledger.exercise(Token.Redeem, tokenCid, { redeemAmount });
+  // }
 
-  async purchaseToken(purchaseDto: PurchaseDto) {
-    const { owner, tokenCid, buyer, purchaseAmount } = purchaseDto;
-    return this.ledger.exercise(Token.Purchase, tokenCid, { buyer, purchaseAmount });
-  }
+  // async purchaseToken(purchaseDto: PurchaseDto) {
+  //   const { owner, tokenCid, buyer, purchaseAmount } = purchaseDto;
+  //   return this.ledger.exercise(Token.Purchase, tokenCid, { buyer, purchaseAmount });
+  // }
 }
